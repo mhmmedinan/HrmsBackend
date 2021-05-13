12ec71f9bc0966433f1396a3c88e91a6e234e12d
mrm65 <minan.65@hotmail.com>
2021-05-13 12:05:23 +0300

    6. Hafta Ödev

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobTitleManager.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.JobTitleService;
+import kodlamaio.hrms.dataAccess.abstracts.JobTitleDao;
+import kodlamaio.hrms.entities.concretes.JobTitle;
+
+@Service
+public class JobTitleManager implements JobTitleService{
+
+	private JobTitleDao jobTitleDao;
+	
+	@Autowired
+	public JobTitleManager(JobTitleDao jobTitleDao) {
+		super();
+		this.jobTitleDao = jobTitleDao;
+	}
+	
+	@Override
+	public List<JobTitle> getAll() {
+		// TODO Auto-generated method stub
+		return this.jobTitleDao.findAll();
+	}
+
+}
